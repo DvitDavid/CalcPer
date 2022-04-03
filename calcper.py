2d93be938b8267122644f9f70991a4bd472d9f0a
DvitDavid <31122094+DvitDavid@users.noreply.github.com>
2022-04-03 11:07:00 +0200

    Fix point
    Made the change of character comma by the dot

--- calcper.py
+++ calcper.py
@@ -22,34 +22,34 @@ class FinestraPrincipal(QMainWindow):
         self.iPreuDte.setText("0")
 
     def calcCost(self):
-        valor1 = float(self.iPreu.text())
-        valor2 = float(self.iPreuDte.text())
+        valor1 = float(self.iPreu.text().replace(",","."))
+        valor2 = float(self.iPreuDte.text().replace(",","."))
         if valor1 == 0 and valor2 == 0:
             self.missatge()
         else:
             self.etiSortRes.setText(str(round((valor1 - valor2)/valor2*100,2))+ "%")
         
     def augmentPer(self):
-        valor1 = float(self.iPreu.text())
-        valor2 = float(self.iPreuDte.text())
+        valor1 = float(self.iPreu.text().replace(",","."))
+        valor2 = float(self.iPreuDte.text().replace(",","."))
         self.etiSortRes.setText(str(round(valor1/(1-(valor2/100)),2))+ "€")
     
     def ferDte(self):
-        valor1 = float(self.iPreu.text())
-        valor2 = float(self.iPreuDte.text())
+        valor1 = float(self.iPreu.text().replace(",","."))
+        valor2 = float(self.iPreuDte.text().replace(",","."))
         dte = valor1*(valor2/100)
         self.etiSortRes.setText(str(round(valor1-dte,2))+ "€")
-                       
+
     def posarIva(self):
-        valor1 = float(self.iPreu.text())
-        valor2 = float(self.iPreuDte.text())   
+        valor1 = float(self.iPreu.text().replace(",","."))
+        valor2 = float(self.iPreuDte.text().replace(",","."))
         if valor2 == 0:
             self.etiSortRes.setText(str(round(valor1*0.21+valor1,2))+ "€")
         else:
             self.etiSortRes.setText(str(round(valor1*(valor2/100)+valor1,2))+ "€")
 
     def treureIva(self):
-        valor1 = float(self.iPreu.text())
+        valor1 = float(self.iPreu.text().replace(",","."))
         self.etiSortRes.setText(str(round(valor1/1.21,2))+ "€")
 
     def missatge(self):
@@ -70,7 +70,6 @@ class FinestraPrincipal(QMainWindow):
         self.iPreu.setText(resolt)
         self.iPreuDte.setText("0")
     
-
 app = QApplication(sys.argv)
 finestra = FinestraPrincipal()
 finestra.show()
